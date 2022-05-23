6f6237841aa51f3b6193fa3d4515006f27a0664c
Bruna <brunadelmouro@gmail.com>
2022-05-22 21:33:32 -0300

    First commit

---
+++ src/main/java/com/brunadelmouro/udemytdd/UdemytddApplication.java
@@ -0,0 +1,13 @@
+package com.brunadelmouro.udemytdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemytddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemytddApplication.class, args);
+	}
+
+}
